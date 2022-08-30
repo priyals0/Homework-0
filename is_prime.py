74531755cd6f9d1d38e041d1d5493afa0688e8e3
priyals0 <112434337+priyals0@users.noreply.github.com>
2022-08-29 22:15:34 -0400

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -3,25 +3,25 @@
 def is_prime(num):
     #num = int(input("> "))
     if(num<2): 
-        print("False")
+        return False
     elif(num%2==0 and num!= 2):
-        print("False")
+        return False
     elif(num%3==0 and num!= 3):
-        print("False")
+        return False
     elif(num%5==0 and num!= 5):
-        print("False")
+        return False
     elif(num%7==0 and num!= 7):
-        print("False")
+        return False
     elif(num%11==0 and num!= 11):
-        print("False")
+        return False
     elif(num%13==0 and num!= 13):
-        print("False")
+        return False
     elif(num%17==0 and num!= 17):
-        print("False")
+        return False
     elif(num%23==0 and num!= 23):
-        print("False")
+        return False
     else:
-        print("True")
+        return True
 
 def main():
     num = int(input("> "))
