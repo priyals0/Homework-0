36ffdd19b41297538347c4f3a7f5ca66664ec3ba
priyals0 <112434337+priyals0@users.noreply.github.com>
2022-09-02 11:36:53 -0400

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,28 +1,15 @@
 #Priyal Shah Problem 4
+import math
 
 def is_prime(num):
     #num = int(input("> "))
     if(num<2): 
         return False
-    elif(num%2==0 and num!= 2):
-        return False
-    elif(num%3==0 and num!= 3):
-        return False
-    elif(num%5==0 and num!= 5):
-        return False
-    elif(num%7==0 and num!= 7):
-        return False
-    elif(num%11==0 and num!= 11):
-        return False
-    elif(num%13==0 and num!= 13):
-        return False
-    elif(num%17==0 and num!= 17):
-        return False
-    elif(num%23==0 and num!= 23):
-        return False
+    for i in range(2, num):
+        if(num%i == 0):
+            return False
     else:
         return True
-
 
 if __name__ == "__main__" :
     while True:
